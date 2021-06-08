346eb525116cb3b7bda21cf97ebd77a4d0e48ccb
OyakiMorse <legenda1200@gmail.com>
2021-06-08 12:33:52 +0300

    create backend

--- server/routes/typeRouter.js
+++ server/routes/typeRouter.js
@@ -1,8 +1,9 @@
 const Router = require('express')
 const router = new Router()
 const typeController = require('../controller/typeController')
+const chekcRole = require('../middleware/checkRoleMiddleware')
 
-router.post('/', typeController.create)
+router.post('/', chekcRole('ADMIN'), typeController.create)
 router.get('/', typeController.getAll)
 
 
